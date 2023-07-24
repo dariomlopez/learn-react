a79dd9dc25eee95f9f346d5774b673c1cc6226fb
César <cesarlpb@gmail.com>
2023-07-24 18:31:48 +0200

    Rutas y Form para crear Post

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,14 +1,55 @@
 // import { useState } from 'react'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import './assets/css/App.css'
 import Home from './components/Home';
-
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Form from './components/Form';
 function App() {
 
   return (
     <>
-      <Home />
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+          <Route path="crear" element={<Form />} />
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </>
   )
 }
 
+const Layout = () => {
+  return(
+  <>
+  <header>
+    <Navbar />
+  </header>
+    <div id="home" className=''>
+      <main>
+    <Outlet />  
+    </main>
+    <Footer />
+  </div>
+  </>)
+}
+
+const NoMatch = () => {
+  return (
+    <div>
+      <h2>404 :(</h2>
+      <p>
+        <Link to="/">Ir a Home</Link>
+      </p>
+    </div>
+  );
+}
 export default App
--- blog/src/components/Home.jsx
+++ blog/src/components/Home.jsx
@@ -6,19 +6,11 @@ import Footer from "./Footer";
 const Home = () => {
   return (
   <>
-  <div id="home" className=''>
-    <header>
-      <Navbar />
-    </header>
-    <main>
-      <Hero />
-      <PostContainer id="1" title="Últimos Posts" />
-      <PostContainer id="2" title="Guías" />
-      <PostContainer id="3" title="Consejos" />
-      <PostContainer id="4" title="Sección sin contenido" />
-      <Footer />
-    </main>
-  </div>
+    <Hero />
+    <PostContainer id="1" title="Últimos Posts" />
+    <PostContainer id="2" title="Guías" />
+    <PostContainer id="3" title="Consejos" />
+    <PostContainer id="4" title="Sección sin contenido" />
   </>
 )};
 
